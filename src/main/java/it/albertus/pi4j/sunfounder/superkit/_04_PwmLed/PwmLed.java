0ec3cbbee82319ecdfedecadc9b925101c64e8c6
Albertus82 <albertus82@gmail.com>
2016-12-18 19:36:49 +0100

    Lesson 4.

---
+++ src/main/java/it/albertus/pi4j/sunfounder/superkit/_04_PwmLed/PwmLed.java
@@ -0,0 +1,28 @@
+package it.albertus.pi4j.sunfounder.superkit._04_PwmLed;
+
+import com.pi4j.io.gpio.GpioController;
+import com.pi4j.io.gpio.GpioFactory;
+import com.pi4j.io.gpio.GpioPinPwmOutput;
+import com.pi4j.io.gpio.RaspiPin;
+
+public class PwmLed {
+
+	public static void main(final String... args) throws InterruptedException {
+		final GpioController gpio = GpioFactory.getInstance();
+
+		final GpioPinPwmOutput ledPin = gpio.provisionPwmOutputPin(RaspiPin.GPIO_01, 0); //pwm output mode
+
+		while (true) {
+			for (int i = 0; i < 1024; i++) {
+				ledPin.setPwm(i);
+				Thread.sleep(2);
+			}
+			Thread.sleep(1000);
+			for (int i = 1023; i >= 0; i--) {
+				ledPin.setPwm(i);
+				Thread.sleep(2);
+			}
+		}
+	}
+
+}
