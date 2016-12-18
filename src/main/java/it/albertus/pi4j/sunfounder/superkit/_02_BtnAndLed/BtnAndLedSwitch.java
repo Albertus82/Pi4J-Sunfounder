5458b1a52439207c5dd2f7adcc63ff8d4b0442df
Albertus82 <albertus82@gmail.com>
2016-12-18 19:27:05 +0100

    Lesson 2 Var. 1

--- src/main/java/it/albertus/pi4j/sunfounder/superkit/_02_BtnAndLed/BtnAndLedSwitch.java
+++ src/main/java/it/albertus/pi4j/sunfounder/superkit/_02_BtnAndLed/BtnAndLedSwitch.java
@@ -6,6 +6,8 @@ import com.pi4j.io.gpio.GpioPinDigitalInput;
 import com.pi4j.io.gpio.GpioPinDigitalOutput;
 import com.pi4j.io.gpio.PinState;
 import com.pi4j.io.gpio.RaspiPin;
+import com.pi4j.io.gpio.event.GpioPinDigitalStateChangeEvent;
+import com.pi4j.io.gpio.event.GpioPinListenerDigital;
 
 public class BtnAndLedSwitch {
 
@@ -15,14 +17,22 @@ public class BtnAndLedSwitch {
 		final GpioPinDigitalOutput ledPin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_00, PinState.HIGH);
 		final GpioPinDigitalInput buttonPin = gpio.provisionDigitalInputPin(RaspiPin.GPIO_01);
 
-		while (true) {
-			if (buttonPin.isLow()) { //indicate that button has pressed down
-				ledPin.setState(PinState.getInverseState(ledPin.getState()));
-				while (buttonPin.isLow()) {
-					Thread.sleep(50);
+		buttonPin.setDebounce(200, PinState.LOW);
+
+		// create and register gpio pin listener
+		buttonPin.addListener(new GpioPinListenerDigital() {
+			@Override
+			public void handleGpioPinDigitalStateChangeEvent(final GpioPinDigitalStateChangeEvent event) {
+				if (event.getState().isLow()) {
+					final PinState oldLedPinState = ledPin.getState();
+					ledPin.setState(PinState.getInverseState(oldLedPinState));
 				}
 			}
-			Thread.sleep(50);
+		});
+
+		// keep program running until user aborts (CTRL-C)
+		while (true) {
+			Thread.sleep(500);
 		}
 	}
 
