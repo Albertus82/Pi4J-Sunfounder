a3cfa09604d6df93c964b071341f7bae8ff33a4a
Albertus82 <albertus82@gmail.com>
2016-12-19 20:10:42 +0100

    shutdown

--- src/main/java/it/albertus/pi4j/sunfounder/superkit/_04_PwmLed/DblPwmLed.java
+++ src/main/java/it/albertus/pi4j/sunfounder/superkit/_04_PwmLed/DblPwmLed.java
@@ -3,6 +3,8 @@ package it.albertus.pi4j.sunfounder.superkit._04_PwmLed;
 import com.pi4j.io.gpio.GpioController;
 import com.pi4j.io.gpio.GpioFactory;
 import com.pi4j.io.gpio.GpioPinPwmOutput;
+import com.pi4j.io.gpio.PinPullResistance;
+import com.pi4j.io.gpio.PinState;
 import com.pi4j.io.gpio.RaspiPin;
 
 public class DblPwmLed {
@@ -13,8 +15,18 @@ public class DblPwmLed {
 	public static void main(final String... args) throws InterruptedException {
 		final GpioController gpio = GpioFactory.getInstance();
 
-		final GpioPinPwmOutput ledPin1 = gpio.provisionPwmOutputPin(RaspiPin.GPIO_01, PWM_MIN_VALUE);
-		final GpioPinPwmOutput ledPin2 = gpio.provisionPwmOutputPin(RaspiPin.GPIO_23, PWM_MAX_VALUE);
+		final GpioPinPwmOutput ledPin1 = gpio.provisionPwmOutputPin(RaspiPin.GPIO_01, PWM_MIN_VALUE); // HW PWM0 OUT 0
+		final GpioPinPwmOutput ledPin2 = gpio.provisionPwmOutputPin(RaspiPin.GPIO_23, PWM_MAX_VALUE); // HW PWM1 OUT 1
+
+		ledPin1.setShutdownOptions(true, PinState.LOW, PinPullResistance.PULL_DOWN); // IN 0
+		ledPin2.setShutdownOptions(true, PinState.LOW, PinPullResistance.PULL_DOWN); // IN 0
+
+		Runtime.getRuntime().addShutdownHook(new Thread() {
+			@Override
+			public void run() {
+				gpio.shutdown();
+			}
+		});
 
 		while (true) {
 			for (int i = PWM_MIN_VALUE; i <= PWM_MAX_VALUE; i++) {
