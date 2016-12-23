7c1cfe79e0203d50f81a0cf1ddd28654d0ae2a22
Albertus82 <albertus82@gmail.com>
2016-12-23 19:33:48 +0100

    Refactoring.

--- src/main/java/it/albertus/pi4j/sunfounder/sensorkit/_30_i2c_lcd1602/I2cLcd1602.java
+++ src/main/java/it/albertus/pi4j/sunfounder/sensorkit/_30_i2c_lcd1602/I2cLcd1602.java
@@ -4,11 +4,11 @@ import com.pi4j.wiringpi.I2C;
 
 public class I2cLcd1602 {
 
-	private static final int LCDAddr = 0x27;
+	private static final int LCD_ADDR = 0x27;
 	private static final int BLEN = 1;
 	private static int fd;
 
-	private static void write_word(int data) {
+	private static void writeWord(final int data) {
 		int temp = data;
 		if (BLEN == 1)
 			temp |= 0x08;
@@ -17,59 +17,59 @@ public class I2cLcd1602 {
 		I2C.wiringPiI2CWrite(fd, temp);
 	}
 
-	private static void send_command(final int comm) {
+	private static void sendCommand(final int comm) {
 		int buf;
 		// Send bit7-4 firstly
 		buf = comm & 0xF0;
 		buf |= 0x04; // RS = 0, RW = 0, EN = 1
-		write_word(buf);
+		writeWord(buf);
 		delay(2);
 		buf &= 0xFB; // Make EN = 0
-		write_word(buf);
+		writeWord(buf);
 
 		// Send bit3-0 secondly
 		buf = (comm & 0x0F) << 4;
 		buf |= 0x04; // RS = 0, RW = 0, EN = 1
-		write_word(buf);
+		writeWord(buf);
 		delay(2);
 		buf &= 0xFB; // Make EN = 0
-		write_word(buf);
+		writeWord(buf);
 	}
 
-	private static void send_data(final int data) {
+	private static void sendData(final int data) {
 		int buf;
 		// Send bit7-4 firstly
 		buf = data & 0xF0;
 		buf |= 0x05; // RS = 1, RW = 0, EN = 1
-		write_word(buf);
+		writeWord(buf);
 		delay(2);
 		buf &= 0xFB; // Make EN = 0
-		write_word(buf);
+		writeWord(buf);
 
 		// Send bit3-0 secondly
 		buf = (data & 0x0F) << 4;
 		buf |= 0x05; // RS = 1, RW = 0, EN = 1
-		write_word(buf);
+		writeWord(buf);
 		delay(2);
 		buf &= 0xFB; // Make EN = 0
-		write_word(buf);
+		writeWord(buf);
 	}
 
 	private static void init() {
-		send_command(0x33); // Must initialize to 8-line mode at first
+		sendCommand(0x33); // Must initialize to 8-line mode at first
 		delay(5);
-		send_command(0x32); // Then initialize to 4-line mode
+		sendCommand(0x32); // Then initialize to 4-line mode
 		delay(5);
-		send_command(0x28); // 2 Lines & 5*7 dots
+		sendCommand(0x28); // 2 Lines & 5*7 dots
 		delay(5);
-		send_command(0x0C); // Enable display without cursor
+		sendCommand(0x0C); // Enable display without cursor
 		delay(5);
-		send_command(0x01); // Clear Screen
+		sendCommand(0x01); // Clear Screen
 		I2C.wiringPiI2CWrite(fd, 0x08);
 	}
 
 	private static void clear() {
-		send_command(0x01); //clear Screen
+		sendCommand(0x01); //clear Screen
 	}
 
 	private static void write(int x, int y, String data) {
@@ -86,16 +86,16 @@ public class I2cLcd1602 {
 
 		// Move cursor
 		addr = 0x80 + 0x40 * y + x;
-		send_command(addr);
+		sendCommand(addr);
 
 		tmp = data.length();
 		for (i = 0; i < tmp; i++) {
-			send_data(data.charAt(i));
+			sendData(data.charAt(i));
 		}
 	}
 
 	public static void main(final String... args) {
-		fd = I2C.wiringPiI2CSetup(LCDAddr);
+		fd = I2C.wiringPiI2CSetup(LCD_ADDR);
 		init();
 		write(0, 0, "Greetings!");
 		write(1, 1, "From SunFounder");
